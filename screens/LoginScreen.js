734fbb8fe6e7f4e4da5b6a5f1b66e7f1a1113d73
Indu Chandana <indu.99.ic@gmail.com>
2023-10-13 12:30:33 +0530

    Oct13 001 Login page animations

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import { View, Text, Image, StatusBar, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 
+import Animated, { FadeInUp, FadeOut, FadeInDown } from 'react-native-reanimated'
+
 const LoginScreen = () => {
     return (
         <View className="bg-white h-full w-full">
@@ -12,10 +14,12 @@ const LoginScreen = () => {
 
             {/* Light */}
             <View className="flex-row justify-around w-full absolute">
-                <Image
+                <Animated.Image
+                    entering={FadeInUp.delay(200).duration(1000).springify()}
                     className="h-[225] w-[90]"
                     source={require('../assets/images/light.png')} />
-                <Image
+                <Animated.Image
+                    entering={FadeInUp.delay(400).duration(1000).springify()}
                     className="h-[160] w-[65]"
                     source={require('../assets/images/light.png')} />
             </View>
@@ -23,38 +27,48 @@ const LoginScreen = () => {
             {/* title & form */}
             <View className="h-full w-full flex justify-around pt-40 pb-10">
                 <View className="flex items-center">
-                    <Text className="text-white font-bold tracking-wider text-5xl">Login</Text>
+                    <Animated.Text
+                        entering={FadeInUp.duration(1000).springify()}
+                        className="text-white font-bold tracking-wider text-5xl">Login</Animated.Text>
                 </View>
 
                 {/* form */}
                 <View className="flex items-center mx-4 space-y-4">
                     {/* Transparent color -> bg-black/5 */}
-                    <View className="bg-black/5 p-2 rounded-2xl w-full">
+                    <Animated.View
+                        entering={FadeInDown.duration(1000).springify()}
+                        className="bg-black/5 p-2 rounded-2xl w-full">
                         <TextInput
                             placeholder='Email'
                             placeholderTextColor={'gray'}
                         />
-                    </View>
-                    <View className="bg-black/5 p-2 rounded-2xl w-full mb-3">
+                    </Animated.View>
+                    <Animated.View
+                        entering={FadeInDown.delay(200).duration(1000).springify()}
+                        className="bg-black/5 p-2 rounded-2xl w-full mb-3">
                         <TextInput
                             placeholder='Email'
                             placeholderTextColor={'gray'}
                             secureTextEntry
                         />
-                    </View>
-                    <View className="w-full">
+                    </Animated.View>
+                    <Animated.View
+                        entering={FadeInDown.delay(400).duration(1000).springify()}
+                        className="w-full">
                         <TouchableOpacity
                             className="w-full bg-sky-400 p-3 rounded-2xl mb-3"
                         >
                             <Text className="text-xl font-bold text-white text-center">Login</Text>
                         </TouchableOpacity>
-                    </View>
-                    <View className="flex-row justify-center">
+                    </Animated.View>
+                    <Animated.View
+                        entering={FadeInDown.delay(600).duration(1000).springify()}
+                        className="flex-row justify-center">
                         <Text className="text-black">Don't have an account?  </Text>
                         <TouchableOpacity>
                             <Text className="text-sky-600">SignUP</Text>
                         </TouchableOpacity>
-                    </View>
+                    </Animated.View>
                 </View>
             </View>
         </View>
